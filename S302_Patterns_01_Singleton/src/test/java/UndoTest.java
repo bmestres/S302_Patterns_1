86be293bc717c7926444eaaaf52df43ac757e344
Bernat Mestres <ber.mestres@gmail.com>
2026-09-03 15:04:40 +0200

    chore: added jUnit dependencies

---
+++ S302_Patterns_01_Singleton/src/test/java/UndoTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class UndoTest {
+
+    @org.junit.jupiter.api.Test
+    void getInstance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addCommand() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void undoCommand() {
+    }
+}
