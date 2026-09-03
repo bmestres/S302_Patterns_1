4a827579d4d2a2da9da2202737710d213f06d654
Bernat Mestres <ber.mestres@gmail.com>
2026-09-03 14:36:24 +0200

    feat: Undo class
    added private constructor, a private instance of itself and a static getInstance() method

--- S302_Patterns_01_Singleton/src/main/java/Undo.java
+++ S302_Patterns_01_Singleton/src/main/java/Undo.java
@@ -1,2 +1,31 @@
+
+import java.util.Stack;
+
 public class Undo {
+
+    private static Undo instance;
+    private Stack<String> commands;
+
+    private Undo(){
+    }
+
+    public static Undo getInstance(){
+        if(instance == null){
+            instance = new Undo();
+        }
+        return instance;
+    }
+
+    public void addCommand(String command){
+
+    }
+
+    public String undoCommand(){
+        return "returnsAndEliminateTheLastCommand";
+    }
+
+    public void showHistory(){
+        // prints all commands stored
+    }
+
 }
