ec95050268267fae6fc13b4cdb10c87805dbcdfe
Bernat Mestres <ber.mestres@gmail.com>
2026-09-17 11:02:04 +0200

    feature: xml report strategy

--- S302_Patterns_03_Strategy/src/main/java/XMLReportStrategy.java
+++ S302_Patterns_03_Strategy/src/main/java/XMLReportStrategy.java
@@ -1,9 +1,31 @@
+import tools.jackson.dataformat.xml.XmlMapper;
+
 public class XMLReportStrategy implements ReportStrategy {
     private static final String DISPLAY_NAME = "XML Report:";
 
     @Override
     public String generateReport(Customer customer) {
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = xmlMapper.writeValueAsString(customer);
+        if(customer == null) {
+            throw new NullPointerException();
+        }
+        return xml;
+    }
 
-        return "";
+
+    static void main(String[] args) {
+
+        Customer customer = new Customer(
+                1,
+                "Customer name",
+                "Customer surname",
+                "email@domain.com",
+                Gender.MALE);
+
+        ReportStrategy xmlReport = new XMLReportStrategy();
+        
+        System.out.println(xmlReport.generateReport(customer));
+
     }
 }
